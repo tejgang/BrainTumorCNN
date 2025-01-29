a0b4329c82e931d8c826ce25b8baef86972ca09d
Tej Gangupantula <tejgang600@gmail.com>
2025-01-29 13:43:53 -0800

    model

---
+++ model.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from config import Config
+
+def build_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(Config.IMAGE_SIZE[0], Config.IMAGE_SIZE[1], 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(Config.NUM_CLASSES, activation='softmax')
+    ])
+
+    return model
